2dd6800557a95635cdedd0eb985027b6d2711c41
MrForester82 <potate82@ya.ru>
2017-05-12 09:19:52 +0300

    1 - Задание 1

--- C-sharp_lab_6/Program.cs
+++ C-sharp_lab_6/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 namespace C_sharp_lab_6
 {
@@ -6,6 +6,20 @@ namespace C_sharp_lab_6
     {
         static void Main(string[] args)
         {
+            double x = 17.28;
+            double y = 0.98;
+
+            Console.WriteLine("До метода sqr.\t\t x = {0}\ty = {1}", x, y);
+
+            Example.sqr(x, y);
+
+            Console.WriteLine("После метода sqr.\t x = {0}\ty = {1}", x, y);
+
+            Example ex = new Example(8);
+
+            Console.WriteLine("В методе Main:\t Obj.x = {0}\tObj.y = {1}", ex.x, ex.y);
+            Example.init(ex);
+            Console.WriteLine("В методе Main:\t Obj.x = {0}\tObj.y = {1}", ex.x, ex.y);
         }
     }
 }
